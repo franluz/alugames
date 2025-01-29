5c6a5e29dac5eabdd9c2666dd09d4cc2d4925db2
francielle oliveira da luz <francielleoliveiradaluz@Airdefrancielle.Princess.local>
2025-01-29 06:01:50 -0300

    Inclusao do mapeamento de assunatura

---
+++ src/main/kotlin/br/com/alura/alugames/modelo/PlanoEntity.kt
@@ -0,0 +1,22 @@
+package br.com.alura.alugames.modelo
+
+import javax.persistence.*
+
+@Entity
+@Table(name="planos")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TipoPlano", discriminatorType = DiscriminatorType.STRING)
+sealed class PlanoEntity(val tipo: String)
+
+@Entity
+@DiscriminatorValue("Avulso")
+class PlanoAvulsoEntity(tipo: String) : PlanoEntity(tipo)
+
+@Entity
+@DiscriminatorValue("Assinatura")
+class PlanoAssinaturaEntity(
+    tipo: String,
+    val mensalidade: Double,
+    val jogosIncluidos: Int,
+    val percentualDescontoReputacao: Double
+) : PlanoEntity(tipo)
