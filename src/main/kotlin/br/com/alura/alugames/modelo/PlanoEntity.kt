3704956ad1cebaacec64922646ba21920624cd64
francielle oliveira da luz <francielleoliveiradaluz@Airdefrancielle.Princess.local>
2025-01-30 05:56:16 -0300

    correcoes e implementacao dao plano

--- src/main/kotlin/br/com/alura/alugames/modelo/PlanoEntity.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/PlanoEntity.kt
@@ -6,17 +6,20 @@ import javax.persistence.*
 @Table(name="planos")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "TipoPlano", discriminatorType = DiscriminatorType.STRING)
-sealed class PlanoEntity(val tipo: String)
+sealed class PlanoEntity(val tipo: String,@Id @GeneratedValue(strategy = GenerationType.IDENTITY) open val id: Int)
 
 @Entity
 @DiscriminatorValue("Avulso")
-class PlanoAvulsoEntity(tipo: String) : PlanoEntity(tipo)
+class PlanoAvulsoEntity(tipo: String="Avulso",
+                        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+                        override val id:Int=0) : PlanoEntity(tipo,id)
 
 @Entity
 @DiscriminatorValue("Assinatura")
 class PlanoAssinaturaEntity(
-    tipo: String,
-    val mensalidade: Double,
-    val jogosIncluidos: Int,
-    val percentualDescontoReputacao: Double
-) : PlanoEntity(tipo)
+    tipo: String ="Assinatura",
+    val mensalidade: Double=0.0,
+    val jogosIncluidos: Int=0,
+    val percentualDescontoReputacao: Double=0.0,
+    override val id: Int=0
+) : PlanoEntity(tipo,id)
